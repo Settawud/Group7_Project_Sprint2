7181d0771f94835b9cf180e684c6e17d2201e920
Ratthasart <ratthasart_piluek@outlook.com>
2025-09-04 19:40:15 +0700

    edit(components) - ProductContent [Color]

--- src/components/organisms/ProductContent.jsx
+++ src/components/organisms/ProductContent.jsx
@@ -83,7 +83,7 @@ const ProductContent = ({ product }) => {
                   ? "border-black"
                   : ""
               }`}
-              style={{ backgroundColor: v.variantOption }}
+              style={{ backgroundColor: v.variantOption[1] }}
             />
           ))}
         </div>
